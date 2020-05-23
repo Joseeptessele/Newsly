b62750a5fc9920dca32fc796339dd11d32c7a735
José Tessele <josetessele@MacBook-Pro-de-Jose.local>
2020-05-23 18:38:46 -0300

    Header components

---
+++ src/components/Header/SmallHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+
+const SmallHeader = ({ title }) => {
+    return (
+        <IonHeader>
+            <IonToolbar 
+                style={{
+                    background: "#f06521",
+                }}
+                color="primary">
+                <IonTitle size="large">{title}</IonTitle>
+            </IonToolbar>    
+        </IonHeader>
+    )
+}
+
+export default SmallHeader;
