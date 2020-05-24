5309b10474a5ed15c491fc1e1b63666461d8e05c
José Tessele <josetessele@MacBook-Pro-de-Jose.local>
2020-05-24 00:57:12 -0300

    Firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1EoLknBQtxjp2XYG2vYEfm38DezkQKCE",
+    authDomain: "newsy-5d2a8.firebaseapp.com",
+    databaseURL: "https://newsy-5d2a8.firebaseio.com",
+    projectId: "newsy-5d2a8",
+    storageBucket: "newsy-5d2a8.appspot.com",
+    messagingSenderId: "987471953946",
+    appId: "1:987471953946:web:5958d944eafd16ee0111a3"
+  };
+
+  export default firebaseConfig
